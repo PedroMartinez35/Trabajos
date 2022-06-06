599c90a1f6461b79c59669fd212eebe3ba56aad2
Pedro Ivan <104878989+PedroMartinez35@users.noreply.github.com>
2022-06-05 23:23:34 -0500

    Proyecto

---
+++ Palabras.js
@@ -0,0 +1,24 @@
+alert('Bienvenido al juego de palabras entrelazadas');
+alert('El juego consta de hacer ingresar palabras con la letra final de una palabra');
+alert('Ejemplo: Avion - Nada');
+alert('La letra inicial es "P"');
+for(let i=0;i<=5;i++)
+{
+    let x;
+    let i;
+
+    let palabras = prompt('Ingresa una palabra');
+    let palabra = prompt('Ingresa una palabra');
+    x=palabras;
+    i=palabra;
+    i
+    if(x==i)
+    {
+        alert('Usted a perdido');
+    }
+}
+
+
+
+
+
